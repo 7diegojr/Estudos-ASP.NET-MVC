cb08affef1d4482f8a657fa7eb091ff8ee7b0cad
7diegojr <dfsntsjunior@gmail.com>
2025-12-18 22:20:25 -0300

    Aula 2 - Criado o Front-End

---
+++ ControleDeContatos/Controllers/ContatoController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControleDeContatos.Controllers
+{
+    public class ContatoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Criar()
+        {
+            return View();
+        }
+
+        public IActionResult ExcluirConfirmacao()
+        {
+            return View();
+        }
+
+        public IActionResult Editar()
+        {
+            return View();
+        }
+
+    }
+}
